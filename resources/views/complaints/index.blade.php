66cca7584af1b86d64eb620b54fae1d055b41206
david3010M <hvaldiviezos@unprg.edu.pe>
2024-12-02 10:06:11 -0500

    Update index.blade.php

--- resources/views/complaints/index.blade.php
+++ resources/views/complaints/index.blade.php
@@ -630,8 +630,6 @@
                         'Content-Type': 'application/json',
                     },
                 })
-                .then(response => response.json())
-                .then(data => console.log(data))
                 .catch(error => console.error('Error:', error));
 
         }
